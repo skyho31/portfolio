01508561a83155decb97e2b957e091012c5bb281
revine kim <skyho31@gmail.com>
2018-08-17 18:16:39 +0900

    new contents 추가

--- src/js/about.js
+++ src/js/about.js
@@ -22,6 +22,8 @@ $('.skillWrap').click(function() {
 if ($(window).width() > 767) {
     var boxOneHeight = $('.right.one').height() + 32;
     var boxTwoHeight = $('.left.one').height() + 32;
+    var boxThreeHeight = $('.right.two').height() + 32;
+    var boxFourHeight = $('.left.two').height() + 32;
 
     $('.hisCircle.two').css('margin-top', boxOneHeight);
     $('.left.one').css('margin-top', boxOneHeight);
@@ -29,10 +31,19 @@ if ($(window).width() > 767) {
     $('.hisCircle.three').css('margin-top', boxOneHeight + boxTwoHeight);
     $('.right.two').css('margin-top', boxTwoHeight + 24);
     //$('.hisLeftBox').css('height', $('.hisRightBox').height());
+
+    $('.hisCircle.four').css('margin-top', boxOneHeight + boxTwoHeight + boxThreeHeight);
+    $('.left.two').css('margin-top', boxThreeHeight + 24);
+    $('.left.two').css('margin-bottom', 60);
+
+    $('.hisRightBox').css('padding-bottom', boxFourHeight);
 } else {
     var boxOneHeight = $('.right.one').height() + 16;
     var boxTwoHeight = $('.hisBoxMobile').height() + 16;
+    var boxThreeHeight = $('.right.two').height() + 16;
 
     $('.hisCircle.two').css('margin-top', boxOneHeight);
     $('.hisCircle.three').css('margin-top', boxOneHeight + boxTwoHeight);
+    $('.hisCircle.four').css('margin-top', boxOneHeight + boxTwoHeight + boxThreeHeight);
+
 }
